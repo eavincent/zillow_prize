c7ba38245c48bd0e5c63fb81631203072c50a0bb
Elizabeth Vincent <liz.vincent92@gmail.com>
2017-09-09 17:00:15 -0400

    Added script for analyses

---
+++ analysis.R
@@ -0,0 +1,26 @@
+# Source data from Kaggle directly so that it's reproducible for the instructors without having to download the data themselves
+
+properties_2016<-read.csv("properties_2016.csv",stringsAsFactors = F)
+# This takes a while...~3 million rows, 58 columns
+str(properties_2016)
+n=nrow(properties_2016)
+
+missingness<-lapply(properties_2016,function(x){
+    ifelse(is.character(x),
+          sum(identical(x,character(0))),
+          sum(is.na(x))
+  )
+})
+
+missingness<-round(unlist(missingness)/n*100,2)
+
+# Handle categorical variables
+# Going to need to handle missing data - either throw it out or impute
+  # Create a colume for "imputed"
+# Use linear model as the baseline - whatever your machine learning algorithm is should do better than a linear model
+# Predict by the mean as a baseline too - if the machine learning algorithm does worse than predicting by the mean, it's incorrect
+
+train<-read.csv("train_2016_v2.csv")
+
+#dictionary<-read.csv("zillow_data_dictionaryl.csv")
+# this is a multi-sheet xls
