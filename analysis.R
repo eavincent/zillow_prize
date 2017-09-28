5618553ae41fd2943b7a884677f1dd609f8dbc16
Elizabeth Vincent <liz.vincent92@gmail.com>
2017-09-28 01:53:42 -0400

    Created .rmd for report, worked on missingness and imputation

--- analysis.R
+++ analysis.R
@@ -1,5 +1,12 @@
 # Source data from Kaggle directly so that it's reproducible for the instructors without having to download the data themselves
 
+library(data.table)
+library(corrplot)
+library(dplyr)
+library(ggplot2)
+library(magrittr)
+
+
 properties_2016<-read.csv("properties_2016.csv",stringsAsFactors = F)
 # This takes a while...~3 million rows, 58 columns
 str(properties_2016)
@@ -13,6 +20,18 @@ missingness<-lapply(properties_2016,function(x){
 })
 
 missingness<-round(unlist(missingness)/n*100,2)
+missingness<-sort(missingness,decreasing=T)
+missingness<-as.data.frame(missingness)
+missingness$var<-row.names(missingness)
+row.names(missingness)<-seq(1:nrow(missingness))
+missingness<-select(missingness,c("var","missingness"))
+
+ggplot(missingness,aes(x=reorder(var, -missingness),y=missingness)) +
+  geom_col()
+
+barplot(as.matrix(m))
+
+M <- cor(properties_2016)
 
 # Handle categorical variables
 # Going to need to handle missing data - either throw it out or impute
@@ -21,6 +40,3 @@ missingness<-round(unlist(missingness)/n*100,2)
 # Predict by the mean as a baseline too - if the machine learning algorithm does worse than predicting by the mean, it's incorrect
 
 train<-read.csv("train_2016_v2.csv")
-
-#dictionary<-read.csv("zillow_data_dictionaryl.csv")
-# this is a multi-sheet xls
